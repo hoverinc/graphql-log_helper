e13afcd1b47effd374a9e5972aa2bdd9f82d9c75
Daniel Deutsch <daniel.deutsch@hover.to>
2021-11-12 08:32:38 -0800

    bundle gem

---
+++ lib/graphql/log_helper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "log_helper/version"
+
+module Graphql
+  module LogHelper
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/graphql/log_helper_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Graphql::LogHelper do
+  it "has a version number" do
+    expect(Graphql::LogHelper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
